494ca62e12866efa496e2ff88ed6b3666ae13855
uriapete <uriarteangelo@gmail.com>
2025-11-08 13:36:17 -0800

    UserGameMaster.j - fix max/min adjustments on wrong answers

--- src/userGameMaster/UserGameMaster.java
+++ src/userGameMaster/UserGameMaster.java
@@ -30,10 +30,19 @@ public class UserGameMaster {
 				System.out.println("Yes! I guessed your number " + computerGuess + " in " + attempts + " attempts.");
 			}
 			else if (answer.equals("too high")) {
-				max = computerGuess + 1;
+				// [1, 100] -> guess = 50 -> too high
+				// therefore, we established that 50 and above are not the answers
+				// thus, the new range should be [1, 49]
+				// therefore, max should be reset to 49 (AKA guess -1)
+				// putting +1 would make the range [1, 51], which puts two numbers in our range that we already know is not the answer
+				max = computerGuess - 1;
 			}
 			else if (answer.equals("too low")) {
-				min = computerGuess - 1;
+				// [1, 100] -> guess = 50 -> too low
+				// therefore, we established that 50 and below are not the answers
+				// thus, the new range should be [51, 100]
+				// therefore, the min should be reset to 51 (AKA guess +1)
+				min = computerGuess + 1;
 			}
 			else { //Will not let you type anything else except the 3 options that are given to you
 				System.out.println("Invalid. Please type 'too high', 'too low', or 'correct'.");
